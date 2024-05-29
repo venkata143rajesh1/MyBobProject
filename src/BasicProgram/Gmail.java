bc5adcdf71613e61695ee8c7e815e8245cf87930
Admin <Admin@DESKTOP-4K4NBNI.bbrouter>
2024-05-29 12:10:37 +0530

    1122Rajesh

---
+++ src/BasicProgram/Gmail.java
@@ -0,0 +1,27 @@
+package BasicProgram;
+
+import org.openqa.selenium.WebDriver;
+
+public class Gmail {
+	public WebDriver driver;
+	
+	public void OpenUrl() {
+		
+	}
+	
+	public void login() {
+		
+	}
+	
+	public void logout() {
+		
+	}
+	
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ src/BasicProgram/TestNG.java
@@ -0,0 +1,23 @@
+package BasicProgram;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+
+public class TestNG {
+	public WebDriver driver;
+  @Test
+  public void f() {
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}

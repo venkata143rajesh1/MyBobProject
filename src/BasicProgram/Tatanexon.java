b9c64102dd0c2d276568b801c23e4d46e9d38403
Admin <Admin@DESKTOP-4K4NBNI.bbrouter>
2024-05-29 12:24:40 +0530

    1122Rajesh

---
+++ src/BasicProgram/Tatanexon.java
@@ -0,0 +1,10 @@
+package BasicProgram;
+
+public class Tatanexon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/BasicProgram/facebook.java
@@ -0,0 +1,10 @@
+package BasicProgram;
+
+public class facebook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
